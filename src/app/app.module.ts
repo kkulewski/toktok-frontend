4902b590056ca6e24c2b7872f0bf2fce2a4bbdd7
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2018-11-04 22:42:09 +0100

    Register HTTP interceptor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { MessageComponent } from './components/message/message.component';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { HttpRouteInterceptor } from './interceptors/http-route-interceptor';
 
 @NgModule({
   declarations: [
@@ -12,7 +14,9 @@ import { MessageComponent } from './components/message/message.component';
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: HttpRouteInterceptor, multi: true }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
