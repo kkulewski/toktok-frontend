b0a53c823b0eef03991212e20a22f56629982116
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2018-11-04 23:15:57 +0100

    Register message service, HttpClient

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,20 +3,36 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { MessageComponent } from './components/message/message.component';
-import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { HttpRouteInterceptor } from './interceptors/http-route-interceptor';
+import { MessageService } from './services/message.service';
 
 @NgModule({
+
   declarations: [
+    // register components here
     AppComponent,
     MessageComponent
   ],
+
   imports: [
-    BrowserModule
+    // register modules here
+    BrowserModule,
+    HttpClientModule,
   ],
+
   providers: [
-    { provide: HTTP_INTERCEPTORS, useClass: HttpRouteInterceptor, multi: true }
+    // register injectable services here
+    MessageService,
+
+    // register interceptors here
+    { provide: HTTP_INTERCEPTORS, useClass: HttpRouteInterceptor, multi: true },
   ],
-  bootstrap: [AppComponent]
+
+  bootstrap: [
+    AppComponent
+  ]
+
 })
+
 export class AppModule { }
