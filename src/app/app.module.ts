764d8eccbf5917afd9ab65f98d5d2744517d537a
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2018-11-04 22:21:47 +0100

    Move message component to components dir

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { MessageComponent } from './message/message.component';
+import { MessageComponent } from './components/message/message.component';
 
 @NgModule({
   declarations: [
