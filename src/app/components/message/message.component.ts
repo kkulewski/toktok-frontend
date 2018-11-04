4e963cb54ff7003c9705a62de2a3ffb6eb643a7d
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2018-11-04 23:15:10 +0100

    Fetch messages and display in component

--- src/app/components/message/message.component.ts
+++ src/app/components/message/message.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MessageService } from 'src/app/services/message.service';
 
 @Component({
   selector: 'app-message',
@@ -7,9 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MessageComponent implements OnInit {
 
-  constructor() { }
+  constructor(private messageSerivce: MessageService) { }
+
+  messages: Models.Message[];
 
   ngOnInit() {
+    this.fetchMessages();
+  }
+
+  private fetchMessages() {
+    this.messageSerivce.getMessages().subscribe(
+      (messages) => { this.messages = messages; },
+      () => { console.log('Cannot fetch messages!'); }
+    );
   }
 
 }
